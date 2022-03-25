50170a4b56f2dd69c67eefeac705a9acae2e1776
jonatasvnascimento <jonatasv.nascimento@gmail.com>
2022-03-25 08:24:26 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LeetCodex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Two_Sum1 teste = new Two_Sum1();
+            teste.soma();
+        }
+    }
+}
---
+++ Two_Sum1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodex
+{
+    public class Two_Sum1
+    {
+        public void soma()
+        {
+            Console.WriteLine("soma");
+        }
+    }
+}
